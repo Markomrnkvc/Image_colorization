52358847909444d8eb16c9ebe054e3009bfc6e46
Marko <marko.marinkovic@study.hs-duesseldorf.de>
2024-12-22 19:39:54 +0100

    adjusting saturation in HSV colorspace manually

--- src/plotting_results.py
+++ src/plotting_results.py
@@ -43,15 +43,41 @@ def colorization(dataset):
 
             # Koloriertes Bild
             colorized_img = im_convert(colorized_images[idx])
-            axes[row, col + 2].imshow(colorized_img)
+
+            #outputs_array = outputs.to("cpu")
+            #outputs_array = outputs_array.numpy()
+            hsv = cv.cvtColor(colorized_img, cv.COLOR_BGR2HSV)
+            if idx == 1:
+                print(hsv[:,:,1])
+                print(hsv[0,0,1])
+                print("------------------------")
+
+                
+
+            #print(hsv[0].mean())
+            #hsv[:,:,2] = hsv[:,:,2]*1.5
+            #hsv[:,:,1] = hsv[:,:,1]*1.5
+            hsv[:,:,0] = hsv[:,:,0]*1.5
+            if idx == 1:
+                print(hsv[:,:,1])
+                print(hsv[0,0,1])
+                print("------------------------")
+                print(hsv[:][:][1].shape)
+                print(hsv[:][:][:][:][:][1])
+                print(hsv.shape)
+
+                #print(hsv)
+            #(1 - kontrast)*
+            #print(hsv[0].shape)
+            rgb = cv.cvtColor(colorized_img, cv.COLOR_HSV2BGR)
+            rgb = cv.cvtColor(colorized_img, cv.COLOR_BGR2RGB)
+            
+            axes[row, col + 2].imshow(rgb)
             axes[row, col + 2].axis('off')
 
         # Entferne Abstände zwischen den Subplots
         plt.subplots_adjust(wspace=0, hspace=0)
         plt.show()
-
-    def rgb_to_gray(img):
-        return img.mean(dim=1, keepdim=True)
 
     def eval_model_and_plot(num_images=num_images):
         try:
@@ -70,9 +96,10 @@ def colorization(dataset):
                 #grayscale_images = rgb_to_gray(images).to(device)
                 grayscale_images = transforms.functional.rgb_to_grayscale(images).to(device)
                 images = images.to(device)
-                print(images.shape)
-                print(grayscale_images.shape)
                 outputs = model(grayscale_images)
+                if dataset == "Cifar10":
+                    outputs = (outputs * 255).to(torch.uint8)
+
                 
                 
                 # Zeige die ersten 'num_images' Bilder an, jeweils 5 pro Reihe
@@ -80,11 +107,14 @@ def colorization(dataset):
                 break  
     
     if dataset == "Cifar10":
-        from CIFAR_colorization import ConvNet, rgb_to_gray, test_dataset, test_loader, train_loader, train_dataset
-        trained_model_path = "./models_Cifar10/model_20241104_232646_18"
-            
+        from CIFAR_colorization import ConvNet,  test_dataset, test_loader, train_loader, train_dataset
+        #trained_model_path = "./models_Cifar10/model_20241104_232646_18"
+        #trained_model_path = "./models_Cifar10/model_20241208_193034_14"
+        #trained_model_path = "./models_Cifar10/model_20241208_181658_49" #das hier ist die Unet structure
+        trained_model_path = "./models_Cifar10/model_20241211_124608_99"
     elif dataset == "Imagenette":
         from Imagenette_colorization import ConvNet, test_dataset, test_loader, train_loader, train_dataset
-        trained_model_path = ".\models_Imagenette\model_20241126_173658_10"
+        #trained_model_path = ".\models_Imagenette\model_20241126_173658_10"
+        trained_model_path = ".\models_Imagenette\model_20241209_005507_7"
 
     eval_model_and_plot(num_images)
