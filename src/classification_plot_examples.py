d916f1e6f7ac5857ed303a4407fb556cd4582ff4
Marko <marko.marinkovic@study.hs-duesseldorf.de>
2024-12-31 00:15:13 +0100

    plotting example image for classification

--- src/classification_plot_examples.py
+++ src/classification_plot_examples.py
@@ -10,10 +10,12 @@ from tqdm import tqdm
 import numpy as np
 import matplotlib.pyplot as plt
 import torchvision
-from CIFAR10_classification import ConvNet, train_dataset, test_dataset, train_loader, test_loader, NUM_CLASSES, EPOCHS, BATCH_SIZE, LEARNING_RATE
+#from CIFAR10_classification import ConvNet, train_dataset, test_dataset, train_loader, test_loader, NUM_CLASSES, EPOCHS, BATCH_SIZE, LEARNING_RATE
+from Imagenette_classification import ConvNet, train_dataset, test_dataset, train_loader, test_loader, NUM_CLASSES, EPOCHS, BATCH_SIZE, LEARNING_RATE
 
 
-TRAINED_MODEL_PATH = "./gpt/model_20241229_001305_409" #ergebnisse nach 6 stunden Training
+#TRAINED_MODEL_PATH = "./models_CIFAR10_classification/model_20241229_001305_409" #ergebnisse nach 6 stunden Training
+TRAINED_MODEL_PATH = "./models_Imagenette_classification/model_20241230_085451_35" #ergebnisse nach 6 stunden Training
 
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
@@ -30,7 +32,9 @@ def plot_images(original, grayscale, colorized):
     original_img = im_convert(original[0])
     grayscale_img = grayscale[0].cpu().numpy()[0]
     colorized_img = im_convert(colorized[0])
+    print(colorized_img.shape)
     fig, ax = plt.subplots(1, 3, figsize=(18, 6))
+    
     ax[0].imshow(original_img)
     ax[0].set_title("Original RGB Image")
     ax[0].axis('off')
@@ -51,7 +55,7 @@ def plot_examples(model= ConvNet(num_classes=NUM_CLASSES)):
 
 
     with torch.no_grad():
-        for i, (images, _) in enumerate(train_loader):
+        for i, (images, _) in enumerate(test_loader):
             grayscale_images = transforms.functional.rgb_to_grayscale(images).to(device)
             images = images.to(device)
             outputs = model(grayscale_images)
@@ -60,6 +64,11 @@ def plot_examples(model= ConvNet(num_classes=NUM_CLASSES)):
             green_classes = green_logits.argmax(dim=1)
             blue_classes = blue_logits.argmax(dim=1)
             colorized_output = torch.stack([red_classes, green_classes, blue_classes], dim=1)
+            print(images.min(), images.max())
+            print(images.shape)
+            print(outputs.min(), outputs.max())
+            print(outputs.shape)
+            images = images.clamp(0,1) #damit imshow nicht cliped später
             plot_images(images, grayscale_images, colorized_output)
             break
 
